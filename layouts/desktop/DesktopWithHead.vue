f5011a5e1be05c9000ea43224670dbb88cabba30
lichong <git@lichong.host>
2024-01-21 16:53:02 +0800

    feat: update default z-index

--- layouts/desktop/DesktopWithHead.vue
+++ layouts/desktop/DesktopWithHead.vue
@@ -3,13 +3,13 @@
     <div class="flex h-[100px] w-full justify-between pl-10 pr-20 pt-5 text-3xl">
       <logo class="w-[230px]"></logo>
       <nav class="w-[calc(100%-440px)]">
-        <ol class="flex h-full w-full items-center justify-center gap-9">
-          <li v-for="(item, index) in navList" :key="index" :class="item.path === route.path ? 'underline decoration-4 underline-offset-8' : ''">
+        <ol class="relative z-10 flex h-full w-full items-center justify-center gap-9">
+          <li v-for="(item, index) in navList" :key="index" :class="item.path === route.path ? 'underline decoration-4 underline-offset-8 ' : ' '">
             <NuxtLink :to="item.path">{{ item.name }}</NuxtLink>
           </li>
         </ol>
       </nav>
-      <div class="flex h-full w-[210px] items-center justify-center">
+      <div class="relative z-10 flex h-full w-[210px] items-center justify-center">
         <template v-if="!user">
           <NuxtLink to="/register">注册</NuxtLink>
           /
