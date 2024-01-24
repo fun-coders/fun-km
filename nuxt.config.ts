a796d5a6b339cf5a0532be4d24eb33d598fab9ad
lichong <git@lichong.host>
2024-01-24 20:51:04 +0800

    feat: config update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,7 @@ export default defineNuxtConfig({
     redirectOptions: {
       login: '/login',
       callback: '/',
-      exclude: ['/register', '/about'],
+      exclude: ['/register', '/about', '/'],
     },
   },
   eslint: {
